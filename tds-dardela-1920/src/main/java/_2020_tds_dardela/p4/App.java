12e8eed260eee3e56c9ea06d5165dce663a06667
Darío de la Torre <dario.torre@alumnos.uva.es>
2019-12-24 16:39:21 +0100

    Creación del proyecto

---
+++ tds-dardela-1920/src/main/java/_2020_tds_dardela/p4/App.java
@@ -0,0 +1,13 @@
+package _2020_tds_dardela.p4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
