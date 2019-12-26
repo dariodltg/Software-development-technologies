790f13dbc3390ad1bad88c7d35f3497a03ac8d0e
Darío de la Torre <dario.torre@alumnos.uva.es>
2019-12-26 12:09:40 +0100

    Creado test y stubs del constructor y getNombre

--- tds-dardela-1920/src/main/java/es/uva/inf/tds/pr4/RedMetro.java
+++ tds-dardela-1920/src/main/java/es/uva/inf/tds/pr4/RedMetro.java
@@ -1,5 +1,7 @@
 package es.uva.inf.tds.pr4;
 
+import es.uva.inf.tds.redmetro.Linea;
+
 /**
  * Implementación de la clase que representa una red de líneas de metro.
  * Práctica 4 de Tecnologías para el Desarrollo de Software.
@@ -8,5 +10,29 @@ package es.uva.inf.tds.pr4;
  *
  */
 public class RedMetro {
+
+	/**
+	 * Constructor de una red de líneas de metro
+	 * 
+	 * @param nombre
+	 *            Nombre de la red
+	 * @param lineas
+	 *            Conjunto de líneas de metro que tendrá la red
+	 * @throws IllegalArgumentException
+	 *             Si la red no tiene al menos dos líneas
+	 */
+	public RedMetro(String nombre, Linea... lineas) {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * Devuelve el nombre de la red de líneas de metro
+	 * 
+	 * @return El nombre de la red de líneas de metro
+	 */
+	public String getNombre() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- tds-dardela-1920/src/test/java/es/uva/inf/tds/pr4/RedMetroTDDTest.java
+++ tds-dardela-1920/src/test/java/es/uva/inf/tds/pr4/RedMetroTDDTest.java
@@ -2,14 +2,55 @@ package es.uva.inf.tds.pr4;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+
+import es.uva.inf.maps.CoordenadasGPS;
+import es.uva.inf.tds.redmetro.Estacion;
+import es.uva.inf.tds.redmetro.Linea;
 
 /**
  * Clase de test TDD para la clase RedMetro
+ * 
  * @author dardela
  *
  */
 class RedMetroTDDTest {
 
+	public static CoordenadasGPS c1;
+	public static CoordenadasGPS c2;
+	public static CoordenadasGPS c3;
+	public static CoordenadasGPS c4;
+	public static Estacion e1;
+	public static Estacion e2;
+	public static Linea l1;
+	public static Linea l2;
+
+	@Before
+	public void setUp() {
+		c1 = new CoordenadasGPS("040°42'46\"N", "074°00'21\"O");
+		c2 = new CoordenadasGPS("040°42'46\"N", "074°00'20\"O");
+		c3 = new CoordenadasGPS("040°42'46\"N", "074°00'19\"O");
+		c4 = new CoordenadasGPS("040°42'46\"N", "074°00'18\"O");
+		e1 = new Estacion("estacion1", c1, c2);
+		e2 = new Estacion("estacion2", c3, c4);
+		l1 = new Linea(1, "rojo", e1, e2);
+		l2 = new Linea(2, "azul", e1, e2);
+	}
+
+	@Test
+	public void testConstructorRedMetro() {
+		RedMetro red = new RedMetro("red1", l1, l2);
+		assertNotNull(red);
+		assertEquals(red.getNombre(), "red1");
+	}
+
+	@Test
+	public void testConstructorNoValido() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			@SuppressWarnings("unused")
+			RedMetro red = new RedMetro("red1", l1);
+		});
+	}
 
 }
