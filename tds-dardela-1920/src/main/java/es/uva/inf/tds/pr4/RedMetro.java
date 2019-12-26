e591e04094f0c3df71b279893a3058b9f7a81bd2
Darío de la Torre <dario.torre@alumnos.uva.es>
2019-12-26 11:31:03 +0100

    Creación de las clases necesarias

---
+++ tds-dardela-1920/src/main/java/es/uva/inf/tds/pr4/RedMetro.java
@@ -0,0 +1,12 @@
+package es.uva.inf.tds.pr4;
+
+/**
+ * Implementación de la clase que representa una red de líneas de metro.
+ * Práctica 4 de Tecnologías para el Desarrollo de Software.
+ * 
+ * @author dardela
+ *
+ */
+public class RedMetro {
+
+}
---
+++ tds-dardela-1920/src/test/java/es/uva/inf/tds/pr4/RedMetroTDDTest.java
@@ -0,0 +1,15 @@
+package es.uva.inf.tds.pr4;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Clase de test TDD para la clase RedMetro
+ * @author dardela
+ *
+ */
+class RedMetroTDDTest {
+
+
+}
