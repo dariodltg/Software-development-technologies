6819b9f50194ab9d6576f7a8cef915bc69e9a344
Darío de la Torre <dario.torre@alumnos.uva.es>
2019-12-30 18:44:14 +0100

    Creada clase de tests en aislamiento

---
+++ tds-dardela-1920/src/test/java/es/uva/inf/tds/pr4/RedMetroIsolationTest.java
@@ -0,0 +1,19 @@
+package es.uva.inf.tds.pr4;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Clase de test en aislamiento para la clase RedMetro
+ * 
+ * @author dardela
+ *
+ */
+
+@Tag("Isolation")
+class RedMetroIsolationTest {
+
+
+}
