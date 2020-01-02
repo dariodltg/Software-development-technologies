73bdfc265822ceb618fffad405804afc9d46ce54
Darío de la Torre <dario.torre@alumnos.uva.es>
2020-01-02 16:37:18 +0100

    Creación de la clase RedMetroWhiteBoxTest

---
+++ tds-dardela-1920/src/test/java/es/uva/inf/tds/pr4/RedMetroWhiteBoxTest.java
@@ -0,0 +1,16 @@
+package es.uva.inf.tds.pr4;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Clase de test en aislamiento de caja blanca para aumentar la cobertura para
+ * la clase RedMetro
+ * 
+ * @author dardela
+ *
+ */
+class RedMetroWhiteBoxTest {
+
+}
